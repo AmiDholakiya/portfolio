0b7fad126b7123f041fe3600077ff4a245e2d62a
Amiben Dholakiya <amidholakiya624@gmail.com>
2023-10-08 14:35:47 -0400

    feat: modela design

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,3 +3,35 @@ export type ProjectState = {
     title: string | undefined;
     technologies: string | undefined;
   };
+
+  export type User = {
+    id:string | any;
+    first_name: string;
+    last_name: string;
+    email: string;
+    title: array<string>;
+    headline: string;
+    description: string;
+    profile_file: string; 
+    resume_link: string;
+    location: string;
+  }
+
+  export type SocialMedia = {
+    id:string;
+    name: string;
+    profile_link: string;
+
+  }
+
+  export type Project = {
+    id: string;
+    title: string;
+    description: string;
+    tag: string;
+    cover_file: string
+    technologies: array<string>;
+    project_type: string;
+    source_code_link: string;
+    project_link: string
+  }
